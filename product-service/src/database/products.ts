988a7888a1b5db813ba239141038800472db6076
ReshetovIsMe <razervv@gmail.com>
2021-07-26 15:27:59 +0300

    [task 5] dbclient connect + get products integration

--- product-service/src/database/products.ts
+++ product-service/src/database/products.ts
@@ -1,4 +1,5 @@
-import productList from './productList.json';
+import dbClient from './dbClient';
+import { QueryResultRow } from 'pg';
 
 export interface IProduct {
     count: number,
@@ -10,11 +11,24 @@ export interface IProduct {
 
 const getAllProducts = async (): Promise<IProduct[]> => {
     try {
-        const productsJson = productList;
-        return productsJson;
+        await dbClient.connect();
+        const ddlResult = await dbClient.query(`
+        select p.id as id, p.title as title, p.price as price, p.description as description, s.count as count
+        from shop.products p inner join shop.stocks s on p.id = s.product_id
+        `);
+        const products: IProduct[] = ddlResult.rows.map((productRow: QueryResultRow) => ({
+            count: productRow['count'],
+            description: productRow['description'],
+            id: productRow['id'],
+            price: productRow['price'],
+            title: productRow['title']
+        }));
+        return products;
     } catch (e) {
         console.error(e);
         throw e;
+    } finally {
+        dbClient.end();
     }
 };
 
